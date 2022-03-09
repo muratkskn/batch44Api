f96ab3248aadc55168ede53628dc7f0d43969987
Murat KESKIN <bir1bire1@gmail.com>
2022-03-09 21:33:57 +0300

    apı

--- src/test/java/get_http_request/GetRequest22.java
+++ src/test/java/get_http_request/GetRequest22.java
@@ -1,40 +1,53 @@
-package get_http_request;
+package get_http_request.day09;
 
+import base_url.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
-import test_data.HerokuTestData;
+import test_data.HerOkuAppTestData;
 
 import java.util.HashMap;
 
-public class GetRequest22 extends HerOkuAppBaseUrl{
+import static io.restassured.RestAssured.given;
 
-
-    /*
-https://restful-booker.herokuapp.com/booking/47
-       {
-           "firstname": "Ali",
-           "lastname": "Can",
-           "totalprice": 500,
-           "depositpaid": true,
-           "bookingdates": {
-               "checkin": "2022-02-01",
-               "checkout": "2022-02-11"
+public class GetRequest22 extends HerOkuAppBaseUrl {
+       /*
+   https://restful-booker.herokuapp.com/booking/47
+          {
+              "firstname": "Ali",
+              "lastname": "Can",
+              "totalprice": 500,
+              "depositpaid": true,
+              "bookingdates": {
+                                "checkin": "2022-02-01",
+                                    "checkout": "2022-02-11"
+             }
           }
-       }
-1) JsonPhat
-2) De-Serialization
-*/
+   1) JsonPhat
+   2) De-Serialization
+   */
 
-@Test
+    @Test
     public void test22(){
+        //1) URL OLUSTUR
+        spec05.pathParams("first", "booking", "second", 33);
 
-    spec05.pathParams("first","booking","second",47);
+        //2) EXPECTED DATA OLUSTUR
+        HerOkuAppTestData expectedObje = new HerOkuAppTestData();
+
+        HashMap<String , Object> expectedTestDataMap = expectedObje.setUpTestData();
+
+        System.out.println("TEST DATA iCiNDEKi EXPECTED DATA: " + expectedTestDataMap);
+        // {firstname=Ali,
+        // bookingdates={
+        //               checkin=2022-02-01,
+        //               checkout=2022-02-11},
+        // totalprice=500,
+        // depositpaid=true,
+        // lastname=Can}
 
 
-    HerokuTestData expectedObje=new HerokuTestData();
-    HashMap<String,Object> expectedTestDataMap= expectedObje.setUpTestData();
-    System.out.println(expectedTestDataMap);
-}
+        Response response = given().spec(spec05).when().get("/")
 
 
-
+    }
 }
