de3642b14d1c258a0e9be33052c55665cea169a1
Murat KESKIN <bir1bire1@gmail.com>
2022-03-11 00:21:23 +0300

    apı

--- src/test/java/test_data/DummyTestData.java
+++ src/test/java/test_data/DummyTestData.java
@@ -42,20 +42,52 @@ public class DummyTestData {
 
         /*
     Status kodun 200 olduğunu,
-   14. Çalışan isminin "Haley Kennedy" olduğunu,
-   Çalışan sayısının 24 olduğunu,
-   Sondan 3. çalışanın maaşının 675000 olduğunu
-   40,21 ve 19 yaslarında çalışanlar olup olmadığını
-   10. Çalışan bilgilerinin bilgilerinin aşağıdaki gibi
+    14. Çalışan isminin "Haley Kennedy" olduğunu,
+    Çalışan sayısının 24 olduğunu,
+    Sondan 3. çalışanın maaşının 675000 olduğunu
+    40,21 ve 19 yaslarında çalışanlar olup olmadığını
+    10. Çalışan bilgilerinin bilgilerinin aşağıdaki gibi
          */
 
         HashMap<String, Object> expectedData = new HashMap<>();
         expectedData.put("statusCode", 200);
-        expectedData.put("ondurduncucalisan", "Haley Kennedy");
+        expectedData.put("ondorduncucalisan", "Haley Kennedy");
         expectedData.put("calisansayisi", 24);
         expectedData.put("sondanucuncucalisaninmaasi", 675000);
         expectedData.put("arananyaslar", yaslar);
         expectedData.put("onuncucalisan", onuncu);
         return expectedData;
+    }
+
+    /*
+       {
+       "name":"Ali Can",
+       "salary":"2000",
+       "age":"40",
+   }
+     */
+    public HashMap<String , Object> setUpRequestBody (){
+
+        HashMap<String, Object> requestBody = new HashMap<>();
+        requestBody.put("name", "Ali Can");
+        requestBody.put("salary", "2000");
+        requestBody.put("age", "40");
+        return requestBody;
+    }
+
+    /*
+       gönderildiğinde,Status kodun 200 olduğunu ve dönen response body nin,
+
+   {
+       "status": "success",
+       "data": {
+       "id":…
+   },
+       "message": "Successfully! Record has been added."
+   }
+     */
+
+    public HashMap<String, Object> setUpExpectedData(){
+
     }
 }
