7a0f07a4b55417fdb8113b533133dd0ddb099664
Murat KESKIN <bir1bire1@gmail.com>
2022-03-12 21:44:47 +0300

    apı

--- src/test/java/base_url/JsonPlaceHolderBaseUrl.java
+++ src/test/java/base_url/JsonPlaceHolderBaseUrl.java
@@ -2,6 +2,7 @@ package base_url;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
 import org.junit.Before;
 
 public class JsonPlaceHolderBaseUrl {
@@ -11,5 +12,12 @@ public class JsonPlaceHolderBaseUrl {
     @Before
     public void setUp(){
         spec04 = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com").build();
+    public JSONObject setUpPutData(){
+        JSONObject expectedRequest= new JSONObject();
+        expectedRequest.put("userId",21);
+        expectedRequest.put("title","Wash the dishes");
+        expectedRequest.put("completed",false);
+        }
+
     }
 }
---
+++ src/test/java/get_http_request/PutRequest01.java
@@ -0,0 +1,26 @@
+package get_http_request;
+
+public class PutRequest01 {
+
+    /*
+https://jsonplaceholder.typicode.com/todos/198 URL ine aşağıdaki body gönerdiğimde
+
+{
+"userId": 21,
+"title": "Wash the dishes",
+"completed": false
+}
+Dönen response un status kodunun 200 ve body kısmının aşağıdaki gibi olduğunu test edin
+{
+"userId": 21,
+"title": "Wash the dishes",
+"completed": false,
+"id": 198
+}
+*/
+
+
+
+
+
+}
